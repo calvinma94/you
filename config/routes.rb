271954b1d176be5aa4a743af235d47369b0cd636
taylorcosta <tfcosta@sfu.ca>
2015-11-11 07:41:21 +0000

    merge conflict resolution

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
-  
+
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
