a8ea01c9006347014c6695010c491471f6909a9c
junbob <junbob@sfu.ca>
2015-12-01 09:14:57 +0000

    fixed session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   end
   
   def create
+    @user = User.new
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
